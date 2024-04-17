f9d15cfc2e49cfc37dc6d2af79db105636c5058b
D4_62972_Udhav <udhavpatil0806@gmail.com>
2024-04-17 16:01:26 +0530

    This keyword

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//this keyword with getter and setter methods
+
+public class Main {
+    public static void main(String[] args) {
+        Student obj = new Student(34,"vitthal",38000.49);
+        obj.display();
+        obj.setName("aniket");
+        obj.display();
+    }
+}
